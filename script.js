b40286edb51cf3a047c8d9369f0088b8ff37b312
Diego Ferrari <degoferrari28@gmail.com>
2023-11-24 22:05:35 -0300

    atualizado

---
+++ script.js
@@ -0,0 +1,34 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const textElements = document.querySelectorAll(".text");
+    const increaseBtn = document.getElementById("increaseBtn");
+    const decreaseBtn = document.getElementById("decreaseBtn");
+ 
+    let currentSize = 2; // Tamanho de fonte inicial em pixels
+    const maxSize = 5;   // Tamanho máximo de fonte permitido
+    const minSize = 2;   // Tamanho mínimo de fonte permitido
+ 
+    // Função para ajustar o tamanho da fonte
+    function adjustFontSize(size) {
+        textElements.forEach(function (element) {
+            element.style.fontSize = size + "rem";
+        });
+    }
+ 
+    // Função para aumentar a fonte
+    increaseBtn.addEventListener("click", function () {
+        if (currentSize < maxSize) {
+            currentSize += 0.5;
+            adjustFontSize(currentSize);
+        }
+    });
+ 
+    // Função para diminuir a fonte
+    decreaseBtn.addEventListener("click", function () {
+        if (currentSize > minSize) {
+            currentSize -= 0.5;
+            adjustFontSize(currentSize);
+        }
+    });
+});
+ 
